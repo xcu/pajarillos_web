1a719553dab4fc26487d5060e67f3fcb091a6d08
Cesar <xcumail@gmail.com>
2013-09-11 11:26:16 +0000

    simplyfing urls

--- chunks/views.py
+++ chunks/views.py
@@ -8,17 +8,18 @@ from utils import convert_date
 def index(request):
   return HttpResponse("Hello, world. You're at the poll index.")
 
-def chunk_details(request, day, month, year, hour, minute):
+def chunk_details(request, day, month, year, hour, minute, gather_amount='0'):
   injector = TimeChunkInjector(MongoClient('localhost', 27017), 'stats', 'time_chunks', index='start_date', flush=False)
   d = datetime(int(year), int(month), int(day), hour=int(hour), minute=int(minute))
   chunk_id = convert_date(d)
   context = dict(zip(('terms', 'user_mentions', 'hashtags', 'num_users', 'num_tweets'), injector.from_db(chunk_id)))
   return render(request, 'chunks/index.html', context)
 
-def chunk_gather(request, day, month, year, hour, minute, gather_amount):
+def chunk_gather(request, day, month, year, hour, minute, gather_amount=''):
   injector = TimeChunkInjector(MongoClient('localhost', 27017), 'stats', 'time_chunks', index='start_date', flush=False)
   d = datetime(int(year), int(month), int(day), hour=int(hour), minute=int(minute))
   chunk_id = convert_date(d)
   context = dict(zip(('terms', 'user_mentions', 'hashtags', 'num_users', 'num_tweets'), injector.from_db(chunk_id)))
+  context['gather_amount'] = gather_amount
   return render(request, 'chunks/index.html', context)
   
