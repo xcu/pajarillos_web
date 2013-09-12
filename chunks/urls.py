90d6abba689684edf3dc4bc4fc2a49835664910b
Cesar <xcumail@gmail.com>
2013-09-12 11:22:55 +0000

    Adding chunk reduction

--- chunks/urls.py
+++ chunks/urls.py
@@ -4,5 +4,6 @@ from chunks import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<day>\d+)/(?P<month>\d+)/(?P<year>\d+)/(?P<hour>\d+)/(?P<minute>\d+)/(?P<gather_amount>\d*)$', views.chunk_details, name='chunk_details'),
+    url(r'^s=(?P<sdate>\d+-\d+-\d+-\d+-\d+)/e=(?P<edate>\d+-\d+-\d+-\d+-\d+)$', views.chunk_details, name='chunk_details'),
+    #url(r'^(?P<day>\d+)/(?P<month>\d+)/(?P<year>\d+)/(?P<hour>\d+)/(?P<minute>\d+)/(?P<gather_amount>\d*)$', views.chunk_details, name='chunk_details'),
 )
--- chunks/views.py
+++ chunks/views.py
@@ -1,25 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from db.injector import TimeChunkInjector
+from db.db_manager import DBManager
 from pymongo import MongoClient
 from datetime import datetime
 from utils import convert_date
 
 def index(request):
-  return HttpResponse("Hello, world. You're at the poll index.")
+  return HttpResponse("Hello, world. You're at the chunk index.")
 
-def chunk_details(request, day, month, year, hour, minute, gather_amount='0'):
-  injector = TimeChunkInjector(MongoClient('localhost', 27017), 'stats', 'time_chunks', index='start_date', flush=False)
-  d = datetime(int(year), int(month), int(day), hour=int(hour), minute=int(minute))
-  chunk_id = convert_date(d)
-  context = dict(zip(('terms', 'user_mentions', 'hashtags', 'num_users', 'num_tweets'), injector.from_db(chunk_id)))
+def chunk_details(request, sdate, edate=''):
+  def parse_date(date_str):
+    day, month, year, hour, minute = [int(i) for i in date_str.split('-')]
+    return year, month, day, hour, minute
+  injector = TimeChunkInjector(DBManager(MongoClient('localhost', 27017), 'stats', 'time_chunks', index='start_date', flush=False))
+  sdate = datetime(*parse_date(sdate))
+  if edate:
+    edate = datetime(*parse_date(edate))
+  context = dict(zip(('terms', 'user_mentions', 'hashtags', 'num_users', 'num_tweets'), get_all_chunks(injector, sdate, edate)))
+  context['num_users'] = len(context['num_users'])
   return render(request, 'chunks/index.html', context)
 
-def chunk_gather(request, day, month, year, hour, minute, gather_amount=''):
-  injector = TimeChunkInjector(MongoClient('localhost', 27017), 'stats', 'time_chunks', index='start_date', flush=False)
-  d = datetime(int(year), int(month), int(day), hour=int(hour), minute=int(minute))
-  chunk_id = convert_date(d)
-  context = dict(zip(('terms', 'user_mentions', 'hashtags', 'num_users', 'num_tweets'), injector.from_db(chunk_id)))
-  context['gather_amount'] = gather_amount
-  return render(request, 'chunks/index.html', context)
-  
+def get_all_chunks(injector, sdate, edate):
+  if not edate:
+    # just one, no need to gather
+    return injector.from_db(convert_date(sdate))
+  return injector.reduce_range(convert_date(sdate), convert_date(edate))
+   
+
