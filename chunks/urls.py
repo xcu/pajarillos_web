2c0f97485f6c94a6cbc0838125b64134c7f315f6
Cesar <xcumail@gmail.com>
2013-09-13 08:38:59 +0000

    Separating urls

--- chunks/urls.py
+++ chunks/urls.py
@@ -4,6 +4,7 @@ from chunks import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^s=(?P<sdate>\d+-\d+-\d+-\d+-\d+)/e=(?P<edate>\d+-\d+-\d+-\d+-\d+)$', views.chunk_details, name='chunk_details'),
+    url(r'^s=(?P<sdate>\d+-\d+-\d+-\d+-\d+)$', views.chunk_details, name='chunk_details'),
+    url(r'^s=(?P<sdate>\d+-\d+-\d+-\d+-\d+)/e=(?P<edate>\d+-\d+-\d+-\d+-\d+)$', views.chunk_range, name='chunk_range'),
     #url(r'^(?P<day>\d+)/(?P<month>\d+)/(?P<year>\d+)/(?P<hour>\d+)/(?P<minute>\d+)/(?P<gather_amount>\d*)$', views.chunk_details, name='chunk_details'),
 )
