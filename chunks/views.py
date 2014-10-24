05e6b1df13739f04a74e2614a42fd9295e8b751b
Cesar <xcumail@gmail.com>
2014-10-24 10:08:56 +0100

    committing whatever was in there, revisit this

--- chunks/views.py
+++ chunks/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from db.db_manager import DBChunkManager
+from db.db_manager import ChunkDB
 from db.chunk import ChunkMgr
 from pymongo import MongoClient
 from datetime import datetime
@@ -14,18 +14,19 @@ def index(request):
   return HttpResponse("Hello, world. You're at the chunk index.")
 
 def chunk_details(request, sdate):
-  db = DBChunkManager(MongoClient('localhost', 27017), 'stats')
+  db = ChunkDB(MongoClient('localhost', 27017), 'stats')
   sdate = datetime(*parse_date(sdate))
-  context = db.get_chunk(sdate)
+  context = db.load_json_from_id(sdate)
   return render(request, 'chunks/index.html', context)
 
 def chunk_range(request, sdate, edate):
   # how are we fetching current_chunk in the chunk_container?
-  db = DBChunkManager(MongoClient('localhost', 27017), 'stats')
+  db = ChunkDB(MongoClient('localhost', 27017), 'stats')
   sdate = datetime(*parse_date(sdate))
   edate = datetime(*parse_date(edate))
-  chunk_objs = [db.load_chunk_from_id(c) for c in db.get_chunk_range(sdate, edate)]
-  context = ChunkMgr().get_top_occurrences(chunk_objs, 20)
+  chunk_mgr = ChunkMgr(xxxxxxxxxxxxx)
+  chunk_objs = [chunk_mgr.load_chunk_obj_from_id(c) for c in db.get_chunk_range(sdate, edate)]
+  context = chunk_mgr.get_top_occurrences(chunk_objs, 20)
   context.update({'tweet_ids': sum((c.num_tweets() for c in chunk_objs)),
                   'users': sum((c.num_users() for c in chunk_objs))})
   return render(request, 'chunks/index.html', context)
